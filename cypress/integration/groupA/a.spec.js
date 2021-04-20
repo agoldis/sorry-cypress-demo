6078f19392e9908dd36c0c5fc49a0f3753b4ce6d
John Doe <john@doe.org>
2021-04-19 20:24:32 -0700

    fix: align items to center for menu flexbox

--- cypress/integration/groupA/a.spec.js
+++
@@ -1,7 +0,0 @@
-describe('Suite A', function () {
-  it('Welcomes you with a very long name abc qeewre wer wer:', function () {
-    cy.visit('/');
-    cy.wait(1000);
-    cy.contains('Gofffogle Search');
-  });
-});
